668793e50907d65c9317d5774f420d875b6d2c61
Alejandro Leonian <66581753+aleleonian@users.noreply.github.com>
2024-03-02 18:49:25 -0500

    test delete implemented

--- controllers/tests.js
+++ controllers/tests.js
@@ -1,5 +1,7 @@
 const testRouter = require('express').Router();
 
+const mongoose = require('mongoose');
+
 // eslint-disable-next-line import/no-extraneous-dependencies
 const jwt = require('jsonwebtoken');
 
@@ -18,11 +20,11 @@ testRouter.post('/', async (request, response, next) => {
       return response.status(401).json({ error: 'token invalid' });
     }
   } catch (error) {
-    console.log("error->", error);
+    console.log('error->', error);
     return next(error);
   }
 
-//   const user = await User.findById(decodedToken.id);
+  //   const user = await User.findById(decodedToken.id);
 
   const newTest = request.body;
 
@@ -37,42 +39,81 @@ testRouter.post('/', async (request, response, next) => {
   }
 });
 
-// testRouter.delete('/:id', async (request, response) => {
-//   if (!request.userId) {
-//     return response.status(401).json({ error: 'token invalid' });
-//   }
+testRouter.delete('/:id', async (request, response) => {
+  console.log('request.params.id->', request.params.id);
 
-//   const isValidObjectId = mongoose.Types.ObjectId.isValid(request.params.id);
+  const isValidObjectId = mongoose.Types.ObjectId.isValid(request.params.id);
 
-//   let objectId;
+  let objectId;
 
-//   if (isValidObjectId) {
-//     objectId = new mongoose.Types.ObjectId(request.params.id);
-//   } else {
-//     // eslint-disable-next-line no-console
-//     console.error('Invalid ObjectId format');
-//     return response.status(400).end();
-//   }
+  if (isValidObjectId) {
+    objectId = new mongoose.Types.ObjectId(request.params.id);
+  } else {
+    // eslint-disable-next-line no-console
+    console.error('Invalid ObjectId format');
+    return response.status(400).end();
+  }
 
+  try {
+    const deletedDocument = await Test.findByIdAndDelete(objectId);
+
+    if (deletedDocument) {
+      // eslint-disable-next-line no-console
+      console.log('Document deleted successfully:', deletedDocument);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Document not found');
+    }
+  } catch (error) {
+    return response.status(401).send(`Error deleting document:${error.message}`);
+  }
+
+  return response.status(204).json('document deleted successfully!');
+});
+
+// testRouter.delete('/id:', async (request, response, next) => {
+//   let decodedToken;
 //   try {
-//     const desiredBlogpost = await Test.findById(objectId);
-
-//     if (desiredBlogpost.user.toString() === request.userId) {
-//       const deletedDocument = await Test.findByIdAndDelete(objectId);
-
-//       if (deletedDocument) {
-//         // eslint-disable-next-line no-console
-//         console.log('Document deleted successfully:', deletedDocument);
-//       } else {
-//         // eslint-disable-next-line no-console
-//         console.log('Document not found');
-//       }
-//     } else {
-//       return response.status(401).json({ error: 'Documents can only be deleted by owners.' });
+//     decodedToken = jwt.verify(request.token, process.env.SECRET);
+//     if (!decodedToken.id) {
+//       return response.status(401).json({ error: 'token invalid' });
 //     }
 //   } catch (error) {
-//     return response.status(401).send(`Error deleting document:${error.message}`);
+//     console.log('error->', error);
+//     return next(error);
 //   }
+
+//   //   const isValidObjectId = mongoose.Types.ObjectId.isValid(request.params.id);
+
+//   //   let objectId;
+
+//   //   if (isValidObjectId) {
+//   //     objectId = new mongoose.Types.ObjectId(request.params.id);
+//   //   } else {
+//   //     // eslint-disable-next-line no-console
+//   //     console.error('Invalid ObjectId format');
+//   //     return response.status(400).end();
+//   //   }
+
+//   //   try {
+//   //     const desiredTest = await Test.findById(objectId);
+
+//   //     if (desiredTest.user.toString() === request.userId) {
+//   //       const deletedDocument = await Test.findByIdAndDelete(objectId);
+
+//   //       if (deletedDocument) {
+//   //         // eslint-disable-next-line no-console
+//   //         console.log('Document deleted successfully:', deletedDocument);
+//   //       } else {
+//   //         // eslint-disable-next-line no-console
+//   //         console.log('Document not found');
+//   //       }
+//   //     } else {
+//   //       return response.status(401).json({ error: 'Documents can only be deleted by owners.' });
+//   //     }
+//   //   } catch (error) {
+//   //     return response.status(401).send(`Error deleting document:${error.message}`);
+//   //   }
 
 //   return response.status(204).json('document deleted successfully!');
 // });
