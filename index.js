eb7a6eb8b8ddfea9809adfb5f5bbef825c98e975
Alejandro Leonian <66581753+aleleonian@users.noreply.github.com>
2024-03-05 12:28:48 -0500

    tasks done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const puppie = require('./utils/puppie');
 const app = require('./app');
 
 // Set up the cron job to run myFunction() every day at 10 am
-cron.schedule('23 20 * * *', () => {
+cron.schedule('30 20 * * *', () => {
   puppie.enqueueAllTests();
 }, { timezone: 'America/Bogota' });
 
